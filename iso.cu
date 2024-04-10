f42bacb838e8a390eb84f2e28cbe82f179568e2d
Sergey Litvinov <slitvinov@gmail.com>
2024-04-10 13:29:36 -0400

    if (Vert)

--- iso.cu
+++ iso.cu
@@ -596,6 +596,8 @@ positional:
   nlost = 0;
   for (j = 0; j < nvert; j++) {
     Found = 0;
+    if (Verbose)
+      fprintf(stderr, "%g %g %g\n", vert[j].x, vert[j].y, vert[j].z);
     needl.lower.x = vert[j].x;
     needl.lower.y = vert[j].y;
     needl.lower.z = vert[j].z;
